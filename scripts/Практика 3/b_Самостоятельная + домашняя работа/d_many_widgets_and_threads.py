25672a1789126f8386b35be2c230a4189389a343
konkole <konstan.kolebovskij2017@yandex.ru>
2023-05-17 18:41:56 +0300

    Практика 17.05

--- scripts/Практика 3/b_Самостоятельная + домашняя работа/d_many_widgets_and_threads.py
+++ scripts/Практика 3/b_Самостоятельная + домашняя работа/d_many_widgets_and_threads.py
@@ -1,3 +1,12 @@
 """
 Реализовать окно, которое будет объединять в себе сразу два предыдущих виджета
 """
+from PySide6 import QtWidgets
+
+
+class Window(QtWidgets.QMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.centralWidget()
