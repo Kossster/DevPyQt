76c3e9fde0d47b633a0c8bcc1f02fd7220343f80
Kossster <konstan.kolebovskij2017@yandex.ru>
2023-05-18 21:54:37 +0300

    Сделал 4 задачи, Практика №2

--- scripts/Практика 2/b_Самостоятельная + домашняя работа/d_eventfilter_settings.py
+++ scripts/Практика 2/b_Самостоятельная + домашняя работа/d_eventfilter_settings.py
@@ -23,14 +23,22 @@ from ui.d_eventfilter_settings import Ui_Form
 
 
 class Window(QtWidgets.QWidget):
+    comboBoxItems = ["Отображение в  dec", "Отображение в  hex", "Отображение в  bin", "Отображение в  oct"]
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self.settings = QtCore.QSettings("MySettings_d_eventfilter")
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        # self.initSignals()
+        self.ui.comboBox.addItems(self.comboBoxItems)
+        self.initSignals()
+        self.loadData()
+
 
     def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
+        """Добавить для dial возможность установки значений кнопками клавиатуры(+ и -),
+            выводить новые значения в консоль"""
         key = event.key()
         current_val = self.ui.dial.value()
         if key == QtGui.Qt.Key.Key_Minus:
@@ -39,6 +47,41 @@ class Window(QtWidgets.QWidget):
             self.ui.dial.setValue(current_val + 1)
         print(self.ui.dial.value())
 
+    def initSignals(self):
+        self.ui.horizontalSlider.valueChanged.connect(self.changeDial)
+        self.ui.dial.valueChanged.connect(self.changeHorizontalSlider)
+        self.ui.dial.valueChanged.connect(self.changeLcdNumber)
+        self.ui.comboBox.currentTextChanged.connect(self.ModeLCDNumber)
+
+    def changeDial(self):
+        """Соединить между собой QDial и QSlider"""
+        self.ui.dial.setValue(self.ui.horizontalSlider.value())
+
+    def changeHorizontalSlider(self):
+        """Соединить между собой QSlider и QDial"""
+        self.ui.horizontalSlider.setValue(self.ui.dial.value())
+
+    def changeLcdNumber(self):
+        """Соединить между собой QDial и QLCDNumber"""
+        self.ui.lcdNumber.display(self.ui.dial.value())
+
+    def ModeLCDNumber(self):
+        if self.ui.comboBox.currentText() == self.ui.comboBox.itemText(0):
+            self.ui.lcdNumber.setDecMode()
+        elif self.ui.comboBox.currentText() == self.ui.comboBox.itemText(1):
+            self.ui.lcdNumber.setHexMode()
+        elif self.ui.comboBox.currentText() == self.ui.comboBox.itemText(2):
+            self.ui.lcdNumber.setBinMode()
+        elif self.ui.comboBox.currentText() == self.ui.comboBox.itemText(3):
+            self.ui.lcdNumber.setOctMode()
+
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+        self.settings.setValue("ComboBoxVal", self.ui.comboBox.currentText())
+        self.settings.setValue("LcdNumberVal", self.ui.lcdNumber.value())
+
+    def loadData(self) -> None:
+        self.ui.comboBox.setCurrentText(self.settings.value("ComboBoxVal", ""))
+        self.ui.lcdNumber.setProperty("intValue", self.settings.value("LcdNumberVal", ""))
 
 
 if __name__ == "__main__":
